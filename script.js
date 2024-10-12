3fe4c27d0e7f431b19d4f807a5688f0153501462
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-12 18:25:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
   // your code here
+  if(s.length === 0) {
+	  return '';
+  }	
+	let spaceIndex = s.indexOf(' ');
+
+    // If no space is found, return the entire string
+    if (spaceIndex === -1) {
+        return s;
+    }
+
+    // Return the substring up to the first space
+    return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
